90caec1f2b7a2ca4ebfcae8fc740034c7402c4ba
seema1611 <seemarajpure16@gmail.com>
2020-07-27 00:26:29 +0700

    ADDED: placed three quantity length temp and volume

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './components/Header.jsx';
+import Home from './components/Home.jsx';
 import appData from './commons/data.json';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Header name={appData.appName} navLinks={appData.navigation}></Header>
       <Route path="/" exact>
+        <Home quantities={appData.quantities}></Home>
       </Route>
     </Router> 
   );
---
+++ src/components/Quantity.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Quantity extends Component {
+
+    shouldComponentUpdate(nextProps) {
+        return (this.props.isActive !== nextProps.isActive)
+    }
+
+    render() {
+        return ( 
+            <button ref={this.props.quantity.name} className={this.props.isActive ? `quantity ${this.props.quantity.name}-active` : `quantity`} 
+                    id={this.props.quantity.name} onClick={this.props.onlick.bind(this, this.props.quantity)}>
+                <div></div>
+                <p>{this.props.quantity.name}</p>
+            </button>
+        );
+    }
+}
+export default Quantity;
