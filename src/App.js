1381478fe123034f866ed04df54f0327da0976b5
seema1611 <seemarajpure16@gmail.com>
2020-07-29 00:33:04 +0700

    ADDED: add style to from and to box

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from "react";
 import Header from "./components/Header";
 import "./css/styles.scss";
-import Main from "./components/MainComponent";
+import MainComponent from "./components/MainComponent";
 
 function App() {
   var message = ["from", "to"];
   var unit = [
     {
       measurementType: "Length",
-      units: ["Metre", "Inch", "Foot"],
+      units: ["Feet", "Inch", "Yard", "Kilometer", "Meter", "Centimeter", "Millimeter", "Micrometer", "Mile"],
     },
     {
       measurementType: "Temperature",
-      units: ["Kelvin", "Celcius", "Fahrenheit"],
+      units: ["Celsius", "Fahrenheit", "Kelvin"],
     },
     {
       measurementType: "Volume",
-      units: ["Litre", "Millilitre", "Gallon"],
+      units:  ["Litres", "Milliliters", "Gallons"],
     },
   ];
 
   return (
     <div className="App">
       <Header />
-      <Main unit={unit} message={message} />
+      <MainComponent unit={unit} message={message} />
     </div>
   );
 }
