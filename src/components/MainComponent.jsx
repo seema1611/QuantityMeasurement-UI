6f1cb4441125fd6c9aa30e3c9f79cdf7db4f4871
seema1611 <seemarajpure16@gmail.com>
2020-07-28 09:05:39 +0700

    ADDED: add dropdownlist of different units

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 import '../styles/MainComponent.css'
-import Quantity from './Quantity.jsx'
+import Quantity from '../components/Quantity.jsx'
+import UnitValues from '../components/UnitValues.jsx'
 
 class MainComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {  
-                        activeQuantity:this.props.quantities[0]
+                        activeQuantity:this.props.quantities[0],
+                        activeUnit: this.props.quantities[0].units[0]
                     }
     }
 
     selectQuantity = (quantity) => {
         this.setState({activeQuantity:quantity})
+    }
+
+    selectUnit = (unit) => {
+        this.setState({activeUnit:unit})
     }
 
     render() {
@@ -21,6 +27,10 @@ class MainComponent extends Component {
                 <span>Choose type</span>
                     <div id="home-content-quantities">
                         {this.props.quantities.map(quantity=><Quantity quantity={quantity} isActive={this.state.activeQuantity.name===quantity.name} key={quantity.name} onlick={this.selectQuantity}></Quantity>)}
+                    </div>
+                    <div id="home-content-values">
+                        <UnitValues unitType="from" units={this.state.activeQuantity.units} onchange={this.selectUnit}></UnitValues>
+                        <UnitValues unitType="to" units={this.state.activeQuantity.units} activeUnit={this.state.activeUnit}></UnitValues>
                     </div>
                 </div>
             </main>
---
+++ src/components/UnitValues.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function unitValues(props) {
+    return ( 
+        <div id="unit">
+            <span>{props.unitType}</span>
+            <input type="number" className="unit-value"/>
+            <select id="unit-types" onChange={props.onchange ? (event) =>props.onchange(event.target.children[event.target.selectedIndex].value) : null}>
+                {props.units.map(unit=> {
+                    if(unit !== props.activeUnit)
+                        return <option value={unit}>{unit}</option>
+                    return null;
+                })}
+            </select>
+        </div>
+    );
+}
+export default unitValues;
