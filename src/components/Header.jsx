189a2b5b5d8eda734c6c4679480c751f7cdf5528
seema1611 <seemarajpure16@gmail.com>
2020-07-28 22:41:04 +0700

    REFACTORED: refactor the css file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
-import '../styles/header.css'
+import '../styles/header.scss'
 
 function Header(props) {
     return ( 
         <header>
             <nav>
                 <div id="logo"><Link to="/" className="nav-link">{props.name}</Link></div>
-                <div className="nav-links"><Link to={props.navLinks.link} className="nav-link">{props.navLinks.name}</Link></div>
+                <div id="nav-links"><Link to={props.navLinks.link} className="nav-link">{props.navLinks.name}</Link></div>
             </nav>
             <article><p>Welcome to Quantity Measurement</p></article>
         </header>
