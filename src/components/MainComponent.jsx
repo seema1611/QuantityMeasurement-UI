c32f8257bff3fb50e982123ce95e0d3a27e516cc
seema1611 <seemarajpure16@gmail.com>
2020-07-29 21:12:26 +0700

    ADDED: add focus to images

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -3,36 +3,45 @@ import Quantity from '../components/Quantity'
 import UnitValues from '../components/UnitValues'
 
 class MainComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          currentQuantity: this.props.unit[0],
-        };
-        this.setQuantity = this.setQuantity.bind(this);
-      }
-    
-      setQuantity(quantity) {
-        this.setState({ currentQuantity: quantity });
-      }
-    
-      componentDidMount() {
-        document.getElementById(this.state.currentQuantity.measurementType).focus();
-      }
-    
-      render() {
-        var quantity = this.props.unit.map((quantity) => (
-          <Quantity quantity={quantity} setQuantity={this.setQuantity} />
-        ));
-        var unitvalues = this.props.message.map((message) => (
-          <UnitValues name={message} units={this.state.currentQuantity.units} />
-        ));
-        return (
-          <div className="home">
-            <div id="home-text">CHOOSE TYPE</div>
-            <div id="quantity">{quantity}</div>
-            <div id="converter">{unitvalues}</div>
-          </div>
-        );
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentQuantity: this.props.unit[0],
+    };
+    this.setQuantity = this.setQuantity.bind(this);
+  }
+
+  setQuantity(quantity) {
+    this.setState({ currentQuantity: quantity });
+  }
+
+  render() {
+    var quantity = this.props.unit.map((quantity) => (
+      <Quantity
+        quantity={quantity}
+        setQuantity={this.setQuantity}
+        isActive={
+          this.state.currentQuantity.measurementType ===
+          quantity.measurementType
+        }
+      />
+    ));
+    var unitValues = this.props.message.map((message) => (
+      <UnitValues
+        name={message}
+        units={this.state.currentQuantity.units}
+        setSelectedUnit={this.setSelectedUnit}
+        selectedUnit={this.state.selectedUnit}
+      />
+    ));
+    return (
+      <div className="home">
+        <div id="home-text">CHOOSE TYPE</div>
+        <div id="quantity">{quantity}</div>
+        <div id="converter">{unitValues}</div>
+      </div>
+    );
+  }
+}
+
 export default MainComponent;
--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
 class Quantity extends Component {
+
   render() {
     return (
       <button
-        id={this.props.quantity.measurementType}
-        onClick={() => this.props.setQuantity(this.props.quantity)}>
+        id={this.props.isActive ? `${this.props.quantity.measurementType}-active` :  this.props.quantity.measurementType}
+        onClick={() => this.props.setQuantity(this.props.quantity)}
+      >
         <div id="quantity-image"></div>
         <div id="quantity-name">{this.props.quantity.measurementType}</div>
       </button>
